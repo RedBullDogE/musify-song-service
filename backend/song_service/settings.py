05825d81bd115c41989491a5f39971824ca21fc1
An Drew <gsb06092000@gmail.com>
2020-10-06 15:25:07 +0300

    backend: fix audio playing in chrome by adding new middleware with additional header

--- backend/song_service/settings.py
+++ backend/song_service/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'songs.middleware.AudioMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
