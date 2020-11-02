f143bc791220acb0e40bb5aa8b4bc7269bc82063
An Drew <gsb06092000@gmail.com>
2020-11-02 14:45:33 +0200

    add vuelidate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
 
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 Vue.config.ignoredElements = [/^ion-/]
